0cde7217714391776b34f21f3cb6045eda192e98
vitaan <vitaanster@gmail.com>
2017-05-07 13:05:54 -0400

    Add files via upload

---
+++ Assignment 7/Classification.py
@@ -0,0 +1,65 @@
+"""
+A simple script that demonstrates how we classify textual data with sklearn.
+Created on Mon Mar  6 15:24:58 2017
+Author: Vitaan
+Write a classification script that trains on reviews_train.txt and achieves the maximum possible accuracy on reviews_test.txt. 
+
+You can use any type of classifier that you want.
+
+You get 2 points for every extra 1 % over 85%. For example, if you get an accuracy of 90%, you will get 10 points.
+
+
+
+"""
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+from sklearn import tree
+from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import linear_model
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.cluster import KMeans
+from sklearn.linear_model import LogisticRegression
+
+#read the reviews and their polarities from a given file
+def loadData(fname):
+    reviews=[]
+    labels=[]
+    f=open(fname)
+    for line in f:
+        review,rating=line.strip().split('\t')  
+        reviews.append(review.lower())    
+        labels.append(int(rating))
+    f.close()
+    return reviews,labels
+
+rev_train,labels_train=loadData('reviews_train.txt')
+rev_test,labels_test=loadData('reviews_test.txt')
+
+
+#Build a counter based on the training dataset
+counter = CountVectorizer()
+counter.fit(rev_train)
+
+
+#count the number of times each term appears in a document and transform each doc into a count vector
+counts_train = counter.transform(rev_train)#transform the training data
+counts_test = counter.transform(rev_test)#transform the testing data
+
+#train classifier
+clf =  RandomForestClassifier(n_estimators=2500, n_jobs=15,criterion="entropy",max_features='log2',random_state=150,max_depth=600,min_samples_split=163)
+
+#train all classifier on the same datasets
+clf.fit(counts_train,labels_train)
+
+#use hard voting to predict (majority voting)
+#pred=clf.predict(counts_test)
+
+#print accuracy
+#print (accuracy_score(pred,labels_test))
+
+
+predicted= clf.predict(counts_test)
+print (accuracy_score(predicted,labels_test))
